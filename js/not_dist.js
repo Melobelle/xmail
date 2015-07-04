c15ea12c2219cca1a4d91d218809f852445cc848
Melobelle <564445201@qq.com>
2015-07-04 15:24:54 +0800

    dd

--- js/not_dist.js
+++ js/not_dist.js
@@ -3,12 +3,38 @@ var session = window.sessionStorage;
 
 $(document).ready(function() {
 	$.ajax({
-		url: '/path/to/file',
-		type: 'default GET (Other values: POST)',
-		dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-		data: {param1: 'value1'},
+		url: URL+'Email/getNeedEmailList',
+		type: 'POST',
+		dataType: 'json',
+		data:""
 	})
-	.done(function() {
+	.done(function(data) {
+		if (data.status) {
+			$.each(data.data, function(index, obj) {
+				var email_id = obj.id;
+				var email_title = obj.title;
+				var email_content = obj.content;
+				var email_from = obj.from;
+				var isdistributed = obj.isdistributed;
+				var create_time = obj.create_time;
+				var update_time = obj.update_time;
+
+				var text = '<tr data-id='+email_id+'>'
+							+'<td class="tc"><input id='+email_id+' type="checkbox"></td>'
+							+'<td>'+email_from+'</td>'
+							+'<td><a href="detail.html" >'+email_title+'</a></td>'
+							+'<td>'+create_time+'</td>'
+							+'<td>'
+                            +'<a>'+isdistributed+'</a>'
+                            +'</td>'
+                            +'<td>'
+                            +'<button class="btn btn-default">删除</button>'
+                            +'</td>'
+                            +'</tr>';
+                $(".result-tab").append(text);
+
+			});
+		}
 		console.log("success");
 	})
 	.fail(function() {
@@ -17,5 +43,19 @@ $(document).ready(function() {
 	.always(function() {
 		console.log("complete");
 	});
+
+	//checkbox全选
+	$(".allchoose").click(function(event) {
+		allchoose();
+	});
+
+
 	
 });
+
+function allchoose(){
+	console.log('test');
+	if ($(".allchoose").checked) {
+	}
+}
+
